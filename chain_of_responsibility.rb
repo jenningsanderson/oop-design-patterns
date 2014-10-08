5eb140bdb2083175a7b92dc073bb517d625404ae
Jennings Anderson <jennings.anderson@colorado.edu>
2014-10-07 23:46:21 -0600

    finished chain of responsibility

--- chain_of_responsibility.rb
+++ chain_of_responsibility.rb
@@ -2,36 +2,49 @@
 # Chain of Responsibility
 #
 #
-# Okay to use flags to determine which piece of the program gets this request
+# Okay to use flags to determine which piece of the program gets this request?
+# => Instead of using flags, I just created new instances of the class
+# 
+# => Not the cleanest, but it works...
 #
-#
-# I almost have an idea of how this works?  I think..
-#
-#
-#
+# => This is better: 
+# => http://www.devinterface.com/blog/en/2010/07/design-patterns-in-ruby-chain-of-responsibility/
+
+
 
 #Abstract Help Menu Handler
 class HelpMenu
-	attr_reader :successor
-	def initiailize(args)
+	attr_reader :sucessor
+	def initialize(args)
 		@successor = args[:successor]
-		@type = args[:type]
 	end
 
 	def help(help_type)
-		if help_type == self.type
+		if help_type == self.class
 			run
 		else
-			successor.help(help_type)
+			@successor.help(help_type)
 		end
 	end
 end
 
+
 #Concrete Help Menus that the program can handle
+class HelpError < HelpMenu
+	def initialize
+		nil #This is just an over-ride
+	end
+
+	def help(args)
+		puts "There is no help available at this level"
+	end
+end
+
 class ProgramHelp < HelpMenu
 	def initialize
-		super(type: 0, successor: 0)
+		super(successor: HelpError.new)
 	end
+
 	def run
 		puts "These are the help options for the Main Program:"
 		puts "\tAbout ... About this program"
@@ -39,10 +52,11 @@ class ProgramHelp < HelpMenu
 	end
 end
 
+
+
 class RoutineHelp < HelpMenu
-	
 	def initialize
-		super(type: 1, successor: 0)
+		super(successor: ProgramHelp.new)
 	end
 
 	def run
@@ -53,6 +67,10 @@ class RoutineHelp < HelpMenu
 end
 
 class ObjectHelp < HelpMenu
+	def initialize
+		super(successor: RoutineHelp.new)
+	end
+
 	def run
 		puts "These are the help options for the object: "
 		puts "\tType........ What type of object am I?"
@@ -61,32 +79,50 @@ class ObjectHelp < HelpMenu
 end
 
 
-
-class MyProgram
-	attr_reader :name
-
-	def initialize(args)
-		@name = args[:name]
-	end
-
-	def run
-		this_routine = MyRoutine.new(3)
+module Helpable
+	def help(type)
+		@helpmenu.help(type)
 	end
 end
 
+
+
+class MyProgram
+	include Helpable
+	attr_reader :name, :this_routine
+
+	def initialize(args)
+		@name = args[:name]
+		@helpmenu = ProgramHelp.new
+	end
+
+	def run
+		@this_routine = MyRoutine.new(3)
+	end
+end
+
+
+
 class MyRoutine
+	include Helpable
 	attr_reader :objects
+	
 	def initialize(num)
 		@objects = []
 		num.times do |index|
 			@objects << MyObject.new
 		end
+		@helpmenu = RoutineHelp.new
 	end
 end
 
-class MyObject
-end
 
+class MyObject
+	include Helpable
+	def initialize
+		@helpmenu = ObjectHelp.new
+	end
+end
 
 
 
@@ -96,8 +132,26 @@ if __FILE__ == $0
 	puts "Initializing an instance of MyProgram..."
 
 	program = MyProgram.new(name: "Test Program")
+	program.run
+
+	puts "The main program caling all versions of Help:"
+	puts "============================================="
+	puts program.help(ProgramHelp)
+	puts program.help(RoutineHelp)
+	puts program.help(ObjectHelp)
+
+	puts "The Routine calling all versions of Help:"
+	puts "========================================"
+	puts program.this_routine.help(ProgramHelp)
+	puts program.this_routine.help(RoutineHelp)
+	puts program.this_routine.help(ObjectHelp)
 
 
+	puts "The Object calling all versions of Help:"
+	puts "========================================"
+	puts program.this_routine.objects.first.help(ProgramHelp)
+	puts program.this_routine.objects.first.help(RoutineHelp)
+	puts program.this_routine.objects.first.help(ObjectHelp)
 
 end
 
