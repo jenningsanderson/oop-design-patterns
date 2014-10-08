5c697b06f104fa9c4f76e7d73f735205d99aa6aa
Jennings Anderson <jennings.anderson@colorado.edu>
2014-10-07 20:05:00 -0600

    adding new stuff to it

--- command_pattern.rb
+++ command_pattern.rb
@@ -1,10 +1,53 @@
 #
 # Command Design Pattern Implementation
+# 
 #
 #
 #
 #
 #
-#
-#
-#
+# 
+
+#4 Separate pieces involved:
+
+#Abstract Command Class:
+class Command
+	def execute()
+		nil
+	end
+
+	def unexecute()
+		nil
+	end
+end
+
+
+#Concrete Commands: 
+class CreateFile
+
+	attr_reader :filename
+
+	def initialize(file_name)
+		@filename = file_name
+
+	def execute
+		File.open(filename, 'wb')
+	end
+
+	def unexecute
+		File.
+	end
+end
+
+class WriteLine
+
+	def initialize(file)
+
+	end
+end
+
+
+
+
+if __FILE__ == $0
+	puts "Running the command pattern example"
